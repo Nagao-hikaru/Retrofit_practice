e829df04ee4dff51cb8752a85d1e208639b78a78
Nagao-hikaru <koyanagi29sushi@gmail.com>
2024-11-05 07:40:07 +0900

    API疎通から反映まで
    sealed interfaceをしようすることでcompose側のviewを判定するロジックでは、when式のelseを書かなくてよくなる。

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,4 +1,33 @@
 package com.example.marsphotos.network
 
-class MarsApiService {
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * Retrofit service object for creating api calls
+ */
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): String
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
 }
--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -19,24 +19,36 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApi.retrofitService
+import com.example.marsphotos.network.MarsApiService
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface MarsUiState {
+    data class Success(val photos: String) : MarsUiState
+    object Error : MarsUiState
+    object Loading : MarsUiState
+}
 
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
     init {
         getMarsPhotos()
     }
 
-    /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MarsPhoto] [List] [MutableList].
-     */
     private fun getMarsPhotos() {
-        marsUiState = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            try {
+                val listResult = retrofitService.getPhotos()
+                marsUiState = MarsUiState.Success(listResult)
+            } catch (e: IOException) {
+                marsUiState = MarsUiState.Error
+            }
+        }
     }
 }
